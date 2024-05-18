2aad6e7a560d8aba0a2f7e1056ab8fa36981855c
MaximusGaming <maximilian@gronenthal.de>
2024-05-18 09:43:43 +0200

    yep

--- SharpTorchSamples/Program.cs
+++ SharpTorchSamples/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using SharpTorch;
-using SharpTorch.Layers;
-using SharpTorch.Losses;
-using SharpTorch.Models;
-
-SquaredAbsoluteLoss squaredAbsoluteLoss = new();
-TestModel m = new();
-
-Trainer<TestModel> trainer = new(m, squaredAbsoluteLoss, [], []);
-await trainer.Terminate();
+Console.WriteLine("lol");
